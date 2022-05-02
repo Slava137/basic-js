2b321d250f0ddaf94aad55e54a88ee3b0b9fc7d2
Slava137 <slava13071995@gmail.com>
2022-05-03 02:14:59 +0300

    feat: add solution for what-season

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    return "'arr' parameter must be an instance of the Array!";
+  }
 
+  if (arr.length === 0) {
+    return [];
+  }
+
+  let result = arr.slice();
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] === '--discard-next' && i === (result.length - 1) || result[i] === '--double-next' && i === (result.length - 1)) {
+      return result.slice(0, result.length - 1);
+    } else if (result[i] === '--discard-prev' && i === 0 || result[i] === '--double-prev' && i === 0) {
+        return result.slice(1, result.length);
+    } else if (result[i] === '--discard-next') {
+        resultHalf = result.slice(0, i);
+        let j = i + 2;
+        result = resultHalf.concat(result.slice(j, result.length));
+      return result;
+    } else if (result[i] === '--discard-prev') {
+        resultHalf = result.slice(0, i - 1);
+        let j = i + 1;
+        result = resultHalf.concat(result.slice(j, result.length)); 
+        return result; 
+    } else if (result[i] === '--double-next') {
+        resultHalf = result.slice(0, i);
+        let j = i + 1;
+        result = resultHalf.concat(j, result.slice(j, result.length));  
+        return result;
+    } else if (result[i] === '--double-prev') {
+      resultHalf = result.slice(0, i);
+      let j = i - 1;
+      result = resultHalf.concat(j, result.slice(i + 1, result.length));  
+      return result;
+    } else {
+      result = result;
+    }
+  }
+}
 module.exports = {
   transform
 };
